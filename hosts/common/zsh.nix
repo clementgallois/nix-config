b3b3c7f03ff664af02b7a1f8b40cecdb44fefbc0
clementgallois <8049428+clementgallois@users.noreply.github.com>
2026-04-17 14:04:21 +0200

    pimp zsh a bit

--- hosts/common/zsh.nix
+++ hosts/common/zsh.nix
@@ -1,17 +1,46 @@
 {
+  # Starship Prompt
+  programs.starship.enable = true;
+  
+  # Smarter cd
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  # Fuzzy finder
+  # Ensure the package is installed system-wide
+  environment.systemPackages = with pkgs; [
+    fzf
+  ];
+
+  programs.fzf = {
+    keybindings = true;
+    fuzzyCompletion = true;
+  };
+
+  # Pay-respect
+  # press "f" to correct last command
+  programs.pay-respects ={
+    enable = true;
+    # if i want this someday
+    #aiIntegration =
+
+    # this is the default but might as well be explicit
+    alias = "f";
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
+
     ohMyZsh = {
       enable = true;
       plugins = [
         "git"
-        "z"
-	"thefuck"
       ];
-      theme = "robbyrussell";
     };
   };
 }
