1254873bccaaf00ff575275d8478458a845894c4
clementgallois <8049428+clementgallois@users.noreply.github.com>
2026-04-17 14:46:21 +0200

    details

--- hosts/common/zsh.nix
+++ hosts/common/zsh.nix
@@ -1,12 +1,22 @@
 { pkgs, ...}:
 {
   # Starship Prompt
-  programs.starship.enable = true;
-  
+  programs.starship= {
+    enable = true;
+    settings = {
+      directory = {
+        # prevent truncation to repo root
+        truncate_to_repo = false;
+      };
+    };
+  };
+
   # Smarter cd
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
+    # replace cd
+    flags = [ "--cmd cd" ];
   };
 
   # Fuzzy finder
@@ -36,6 +46,18 @@
     enableCompletion = true;
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      la = "ls -la";
+      #update = "sudo nixos-rebuild switch --flake";
+    };
+
+    histSize = 10000;
+    histFile = "$HOME/.zsh_history";
+    setOptions = [
+      "HIST_IGNORE_ALL_DUPS"
+    ];
 
     ohMyZsh = {
       enable = true;
