5e34fc37727b604a09b8c9196aa2498bc6ccee2a
clementgallois <8049428+clementgallois@users.noreply.github.com>
2026-04-20 10:19:43 +0000

    add project config

---
+++ .lazy.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"folke/snacks.nvim",
+		opts = {
+			picker = {
+				sources = {
+					explorer = {
+						expand_all_folder_in_tree = true,
+						hidden = true,
+					},
+					files = {
+						hidden = true,
+					},
+				},
+			},
+		},
+	},
+}
