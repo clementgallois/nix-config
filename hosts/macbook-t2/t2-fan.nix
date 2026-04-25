7947994818114904a28e1740250d850069504af9
clementgallois <8049428+clementgallois@users.noreply.github.com>
2026-04-25 18:01:20 +0200

    fan speed isn't an option, change to logarithmic

--- hosts/macbook-t2/t2-fan.nix
+++ hosts/macbook-t2/t2-fan.nix
@@ -7,23 +7,23 @@
       # https://github.com/GnomedDev/T2FanRD/blob/master/README.md#nixos-1
       # for speed value, rest is "will see" type of conf"
       fan1 = {
-        min_speed = 1250;
-        max_speed = 6336;
+        # min_speed = 1250;
+        # max_speed = 6336;
 
-        low_temp = 50;
-        high_temp = 85;
-        speed_curve = "exponential";
-        always_full_speed = false;
+        low_temp = 55; # default
+        high_temp = 75; # default
+        speed_curve = "logarithmic";
+        always_full_speed = false; # default
       };
 
       fan2 = {
-        min_speed = 1350;
-        max_speed = 6864;
+        # min_speed = 1350;
+        # max_speed = 6864;
 
-        low_temp = 50;
-        high_temp = 85;
-        speed_curve = "exponential";
-        always_full_speed = false;
+        low_temp = 55; # default
+        high_temp = 75; # default
+        speed_curve = "logarithmic";
+        always_full_speed = false; # default
       };
     };
   };
